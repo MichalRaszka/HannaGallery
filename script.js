103e564abd0b3775555ac03cb1502874afd6ef20
Michal <michal.raszka@outlook.com>
2021-08-16 19:12:24 +0200

    responsive changes

--- script.js
+++ script.js
@@ -82,22 +82,13 @@ anime({
   });
 
 
-  //Animations on hover   
-
-  var buttonEl = document.querySelector('.square');
-
-  function animateButton(scale, duration, elasticity) {
-    anime.remove(buttonEl);
-    anime({
-      targets: buttonEl,
-      scale: scale,
-      duration: duration,
-      elasticity: elasticity
-    });
-  }
-  
-  function enterButton() { animateButton(1.2, 800, 400) };
-  function leaveButton() { animateButton(1.0, 600, 300) };
-  
-  buttonEl.addEventListener('mouseenter', enterButton, false);
-  buttonEl.addEventListener('mouseleave', leaveButton, false);
+  anime({
+    targets: '#s116',
+    scale: [
+      1, 1.15
+    ],
+    loop: true,
+    duration: 500,
+    direction: 'alternate',
+    delay: 2100
+  });
